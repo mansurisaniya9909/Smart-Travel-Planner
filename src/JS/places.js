03d8e9ceebbe4df32c46a1162b317365c8176156
mansurisaniya9909 <mansurisaniya9909@gmail.com>
2026-01-18 22:19:50 +0530

    day8 done

--- src/JS/places.js
+++ src/JS/places.js
@@ -3,7 +3,7 @@ const city = params.get("city");
 
 const cityName = document.querySelector("#cityName");
 const loading = document.querySelector("#loading");
-const placesBox = document.querySelector("#placesBox");
+const placesBox = document.querySelector("#placeBox");
 const errors = document.querySelector("#error");
 
 let openweathermap_apikey = "bd1a9c62c57a999fdf90e514baaac1ee"; //lat and lon
@@ -21,7 +21,7 @@ if (!city) {
     try {
       //get lat and lon from openweathermap api
       const geoRes = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${openweathermap_apikey}`
+        `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${openweathermap_apikey}`,
       );
 
       const geoData = await geoRes.json();
@@ -33,45 +33,59 @@ if (!city) {
       // Get places from geoapify
 
       const placesRes = await fetch(
-        `https://api.geoapify.com/v2/places?categories=tourism.sights&filter=circle:${lon},${lat},12000&limit=12&apiKey=${geoapify_apikey}`
+        `https://api.geoapify.com/v2/places?categories=tourism.sights&filter=circle:${lon},${lat},12000&limit=12&apiKey=${geoapify_apikey}`,
       );
 
       if (!placesRes.ok) throw new Error("Places API failed");
 
       const data = await placesRes.json();
-      console.log(data);
-      
+      // console.log(data);
+
       loading.classList.add("hidden");
 
-      if(!data.features.length) throw new Error("No Places Found");
-      placesBox.innerHTML="";
+      if (!data.features.length) throw new Error("No Places Found");
 
-      for(let place of data.features){
+      placesBox.classList.remove("hidden");
+      placesBox.innerHTML = "";
+
+      for (let place of data.features) {
         let imageUrl = `https://placehold.co/400x300?text=Tourist+Spot`;
-        try{
-            const query = place.properties.name? place.properties.name + " " +city+ " tourism":city + " tourism";
 
-            const unsplashRes = await fetch(
-                `https://api.unsplash.com/search/photos?query=${query}&per_page=1&client_id=${unsplash_apikey}`
-            );
-            const unsplashData = await unsplashRes.json();
-            console.log(unsplashData);
+        try {
+          const query = place.properties.name
+            ? place.properties.name + " " + city + "tourism"
+            : city + "tourism";
 
-            if(unsplashData.results && unsplashData.results.length>0){
-                imageUrl = unsplashData.results[0].urls.small;
-            }
+          const unsplashRes = await fetch(
+            `https://api.unsplash.com/search/photos?query=${query}&per_page=1&client_id=${unsplash_apikey}`,
+          );
 
-            
+          const unsplashData = await unsplashRes.json();
+          console.log(unsplashData);
+
+          if (unsplashData.results && unsplashData.results.length > 0) {
+            imageUrl = unsplashData.results[0].urls.small;
+          }
+        } catch (error) {
+          console.log(error, "Unsplash image not found");
         }
-        catch(error){
-            console.log(error, "Unsplash image not found");
-            
-        }
+
+        // console.log(data);
+
+        let div = document.createElement("div");
+
+        div.className =
+          "bg-white rounded-xl shadow hover:shadow-lg transition hover:scale-125 overflow-hidden";
+
+        div.innerHTML = `<img src="${imageUrl}" alt="${place.properties.name}" class="w-full h-40 object-cover"/>
+
+      <div class="p-4">
+        <h3 class="font-bold text-lg text-green-700 mb-1">${place.properties.name || "Tourist Place"}</h3>
+        <P class="text-sm text-gray-600">${place.properties.city || city}</p>
+      </div>`;
+        placesBox.appendChild(div);
       }
       console.log(data);
-
-      
-      
     } catch (error) {
       console.log("Places Error:", error.message);
       loading.classList.add("hidden");
